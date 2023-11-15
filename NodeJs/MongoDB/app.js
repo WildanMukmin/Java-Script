fdd1c4c1030c4bd2e7fd299140f24f9153f0c4c3
WildanMukmin <wildanmukmin26@gmail.com>
2023-11-15 11:03:12 +0700

    mulai project

--- NodeJs/MongoDB/app.js
+++ NodeJs/MongoDB/app.js
@@ -1,1 +1,30 @@
-console.log("hallo");
+// setting express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// setting mongose
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://wildan:wildan123@contact.yubovxi.mongodb.net/");
+const UserSchema = new mongoose.Schema({
+    nama: "string",
+    nohp: "string",
+    email: "string",
+});
+const UserModel = mongoose.model("Users", UserSchema);
+
+// setting routes
+app.get("/", (req, res) => {
+    UserModel.find({})
+        .then((user) => {
+            res.send(user);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
+// setting server
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
